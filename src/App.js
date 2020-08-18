511ec6c899ce9b25dee7825adea400a8c64cefd0
lie16 <yonattan_hope@yahoo.com>
2020-08-18 09:24:24 +0800

    SSH attemp 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My github page running, SSH attemp
+          My github page running, SSH attemp 2
         </p>
         <a
           className="App-link"
